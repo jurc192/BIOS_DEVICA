a8564edccc7febbec2ae2464f3330f7a9134e7c4
jurc192 <vidmarjur@gmail.com>
2018-08-29 13:26:45 +0200

    Fixed gitignore

---
+++ inc/config.h
@@ -0,0 +1,27 @@
+/*
+    BIOS_device
+    config.h
+
+    BIOS_device configuration file
+*/
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* SETTINGS ******************************************************************/
+
+#define HACKS_DIR               "/home/jure/Projects/Hack_platform/DEV_programs/HACKS_REPO"
+#define SKELETON_DIR            "/home/jure/Projects/Hack_platform/DEV_programs/Skeleton_files"
+
+/*****************************************************************************/
+
+/* Optional / visual settings */
+#define MENU_WIDTH              30
+#define GLOBAL_PADDING          2
+
+#define STATUSBAR_HEIGHT        7
+#define STATUSBAR_PADDING       3
+
+#define ESCDELAY    0
+
+
+#endif
